110b02d6fec5bea3531e371ca608c049d5460015
Mehmet Enes Turhan <eden.turhann@gmail.com>
2024-12-21 02:13:13 +0300

    before the setup of sanity

--- hypejungle/next.config.ts
+++ hypejungle/next.config.ts
@@ -1,7 +1,16 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+    //remove in production because it's not safe
+    images: {
+        dangerouslyAllowSVG: true,
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "*",
+            },
+        ],
+    },
+}
 
-export default nextConfig;
+export default nextConfig
--- hypejungle/src/app/page.tsx
+++ hypejungle/src/app/page.tsx
@@ -1,4 +1,5 @@
 import SearchForm from "@/components/SearchForm"
+import ThingCard from "@/components/ThingCard"
 
 export default async function Home({
     searchParams,
@@ -6,6 +7,22 @@ export default async function Home({
     searchParams: Promise<{ query: string }>
 }) {
     const { query } = await searchParams
+    const posts = [
+        {
+            _createdAt: "Yesterday",
+            views: 100,
+            author: {
+                _id: 1,
+                name: "John Doe",
+            },
+            _id: 1,
+            description:
+                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae!",
+            image: "https://images.unsplash.com/photo-1719937206098-236a481a2b6d?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            category: "Robots",
+            title: "Lorem ipsum",
+        },
+    ]
     return (
         <>
             <section className="pink_container">
@@ -17,6 +34,23 @@ export default async function Home({
                     Beatae!
                 </p>
                 <SearchForm query={query} />
+            </section>
+            <section className="section_container">
+                <p className="text-30-semibold">
+                    {query ? `Search results for ${query}` : "All Things"}
+                </p>
+                <ul className="mt-7 card_grid">
+                    {posts?.length > 0 ? (
+                        posts.map((post) => (
+                            <ThingCard
+                                key={post._id}
+                                post={post}
+                            />
+                        ))
+                    ) : (
+                        <p className="no-results">No things found</p>
+                    )}
+                </ul>
             </section>
         </>
     )
