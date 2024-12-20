9641e1b86b5973ca0397b56a8d18951d6ecb46d7
Mehmet Enes Turhan <eden.turhann@gmail.com>
2024-12-21 00:38:49 +0300

    basic styles and created hero & search input added

--- hypejungle/src/app/layout.tsx
+++ hypejungle/src/app/layout.tsx
@@ -1,5 +1,58 @@
+import localFont from "next/font/local"
 import "@/assets/globals.css"
 import Navbar from "@/components/Navbar"
+
+const workSans = localFont({
+    src: [
+        {
+            path: "../assets/fonts/WorkSans-Black.ttf",
+            weight: "900",
+            style: "normal",
+        },
+        {
+            path: "../assets/fonts/WorkSans-ExtraBold.ttf",
+            weight: "800",
+            style: "normal",
+        },
+        {
+            path: "../assets/fonts/WorkSans-Bold.ttf",
+            weight: "700",
+            style: "normal",
+        },
+        {
+            path: "../assets/fonts/WorkSans-SemiBold.ttf",
+            weight: "600",
+            style: "normal",
+        },
+        {
+            path: "../assets/fonts/WorkSans-Medium.ttf",
+            weight: "500",
+            style: "normal",
+        },
+        {
+            path: "../assets/fonts/WorkSans-Regular.ttf",
+            weight: "400",
+            style: "normal",
+        },
+        {
+            path: "../assets/fonts/WorkSans-Black.ttf",
+            weight: "900",
+            style: "normal",
+        },
+        {
+            path: "../assets/fonts/WorkSans-Thin.ttf",
+            weight: "200",
+            style: "normal",
+        },
+        {
+            path: "../assets/fonts/WorkSans-ExtraLight.ttf",
+            weight: "100",
+            style: "normal",
+        },
+    ],
+    variable: "--font-work-sans",
+})
+
 export default function RootLayout({
     children,
 }: Readonly<{
@@ -15,7 +68,7 @@ export default function RootLayout({
                 />
                 <title>Hype Jungle</title>
             </head>
-            <body>
+            <body className={workSans.variable}>
                 <Navbar />
                 <main>{children}</main>
             </body>
--- hypejungle/src/app/page.tsx
+++ hypejungle/src/app/page.tsx
@@ -1,7 +1,23 @@
-export default function Home() {
+import SearchForm from "@/components/SearchForm"
+
+export default async function Home({
+    searchParams,
+}: {
+    searchParams: Promise<{ query: string }>
+}) {
+    const { query } = await searchParams
     return (
-        <div>
-            <h1 className="text-2xl">Hello</h1>
-        </div>
+        <>
+            <section className="pink_container">
+                <h1 className="heading">
+                    Lorem ipsum dolor sit amet. <br /> Lorem, ipsum dolor.
+                </h1>
+                <p className="sub-heading !max-w-3xl">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Beatae!
+                </p>
+                <SearchForm query={query} />
+            </section>
+        </>
     )
 }
