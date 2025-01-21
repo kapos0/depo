349e02efb78e05d0b7257704ce8eaebf88af7c78
Mehmet Enes Turhan <eden.turhann@gmail.com>
2025-01-22 01:07:57 +0300

    little things start and authjs added

--- handydictionary/src/lib/saltAndHashPassword.ts
+++
@@ -1,9 +0,0 @@
-import bcrypt from "bcryptjs";
-
-export const saltAndHashPassword = async (
-    password: string
-): Promise<string> => {
-    const salt = await bcrypt.genSalt(10);
-    const hash = await bcrypt.hash(password, salt);
-    return hash;
-};
