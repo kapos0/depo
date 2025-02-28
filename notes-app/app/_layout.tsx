cbe162b0400530d6f612e9ab76bda1aae746d5e6
Mehmet Enes Turhan <eden.turhann@gmail.com>
2025-02-28 23:39:01 +0300

    main page and notes page created

--- notes-app/app/_layout.tsx
+++ notes-app/app/_layout.tsx
@@ -1,5 +1,21 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+    return (
+        <Stack
+            screenOptions={{
+                headerStyle: { backgroundColor: "#ff8c00" },
+                headerTintColor: "#fff",
+                headerTitleStyle: { fontWeight: "bold", fontSize: 20 },
+                contentStyle: {
+                    paddingHorizontal: 10,
+                    paddingTop: 10,
+                    backgroundColor: "#f0f0f0",
+                },
+            }}
+        >
+            <Stack.Screen name="index" options={{ title: "Home" }} />
+            <Stack.Screen name="notes" options={{ headerTitle: "Notes" }} />
+        </Stack>
+    );
 }
