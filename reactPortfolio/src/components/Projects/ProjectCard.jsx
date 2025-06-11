19b47b0edc2aefd15058015d57dd6fd668b107ad
Mehmet Enes Turhan <eden.turhann@gmail.com>
2025-06-12 02:22:36 +0300

    Add TypeScript declaration for CSS modules

--- reactPortfolio/src/components/Projects/ProjectCard.jsx
+++ reactPortfolio/src/components/Projects/ProjectCard.jsx
@@ -14,23 +14,27 @@ export function ProjectCard({
             <h3 className={styles.title}>{title}</h3>
             <p className={styles.description}>{description}</p>
             <ul className={styles.skills}>
-                {skills.map((skill, id) => {
-                    return (
-                        <li key={id} className={styles.skill}>
-                            {skill}
-                        </li>
-                    );
-                })}
+                {skills.map((skill, id) => (
+                    <li key={id} className={styles.skill}>
+                        {skill}
+                    </li>
+                ))}
             </ul>
             <div className={styles.links}>
                 <a
-                    href={demo.source}
-                    target={demo.download && "_blank"}
+                    href={demo?.source || demo}
+                    target="_blank"
+                    rel="noopener noreferrer"
                     className={styles.link}
                 >
                     Demo
                 </a>
-                <a href={source} target="_blank" className={styles.link}>
+                <a
+                    href={source}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className={styles.link}
+                >
                     Kaynak
                 </a>
             </div>
--- reactPortfolio/src/components/Projects/Projects.jsx
+++ reactPortfolio/src/components/Projects/Projects.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import styles from "./Projects.module.css";
 
@@ -6,11 +6,25 @@ import projects from "../../data/projects.json";
 import { ProjectCard } from "./ProjectCard";
 
 export function Projects() {
+    const [featuredProjects, setFeaturedProjects] = useState([]);
+    const [otherProjects, setOtherProjects] = useState([]);
+    useEffect(()=>{
+        const featured = projects.filter(project => project.IsFeatured === "true");
+        const others = projects.filter(project => project.IsFeatured === "false");
+        setFeaturedProjects(featured);
+        setOtherProjects(others);
+    },[projects])
     return (
         <section className={styles.container} id="projects">
-            <h2 className={styles.title}>Projelerim</h2>
+            <h2 className={styles.divider}>Projelerim</h2>
             <div className={styles.projects}>
-                {projects.map((project, id) => {
+                {featuredProjects.map((project, id) => {
+                    return <ProjectCard key={id} project={project} />;
+                })}
+            </div>
+            <h2 className={styles.divider}>Klon Projeler</h2>
+            <div className={styles.projects}>
+                {otherProjects.map((project, id) => {
                     return <ProjectCard key={id} project={project} />;
                 })}
             </div>
