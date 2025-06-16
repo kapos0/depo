f938617567a2d235a6f946cd6327fdcf41a0726d
Mehmet Enes Turhan <eden.turhann@gmail.com>
2025-06-17 01:44:30 +0300

    before the backend

---
+++ habit-tracker/app/(tabs)/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+    return (
+        <View>
+            <Text>Welcome to the Home Page!</Text>
+            <Text>Navigate to the Login page from here.</Text>
+        </View>
+    );
+}
