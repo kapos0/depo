5965824e00668073cdaf7f70aa529ca55ed56d40
Mehmet Enes Turhan <eden.turhann@gmail.com>
2025-06-18 17:51:23 +0300

    implement habit fetching and real-time updates; enhance layout with gesture handler

--- habitiko/app/(tabs)/index.tsx
+++ habitiko/app/(tabs)/index.tsx
@@ -1,19 +1,123 @@
+import {
+    client,
+    DATABASE_ID,
+    databases,
+    HABITS_COLLECTION_ID,
+    RealtimeResponse,
+} from "@/lib/appwrite";
 import { useAuth } from "@/lib/auth-context";
+import { Habit } from "@/lib/databaseTypes";
+import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
 import { useRouter } from "expo-router";
-import { StyleSheet, Text, View } from "react-native";
-import { Button } from "react-native-paper";
+import { useCallback, useEffect, useState } from "react";
+import { ScrollView, StyleSheet, View } from "react-native";
+import { Query } from "react-native-appwrite";
+//import Swipeable from "react-native-gesture-handler/ReanimatedSwipeable";
+import { Button, Surface, Text } from "react-native-paper";
 
 export default function Index() {
     const router = useRouter();
     const { user, signOut, isLoadingUser } = useAuth();
     if (!user && !isLoadingUser) router.push("/auth");
+
+    const [habits, setHabits] = useState<Habit[]>([]);
+
+    const fetchHabits = useCallback(async () => {
+        try {
+            const response = await databases.listDocuments(
+                DATABASE_ID,
+                HABITS_COLLECTION_ID,
+                [Query.equal("user_id", user?.$id ?? "")]
+            );
+            if (response.documents.length > 0)
+                setHabits(response.documents as Habit[]);
+            else setHabits([]);
+        } catch (error) {
+            console.error("Error fetching habits:", error);
+        }
+    }, [user]);
+
+    useEffect(() => {
+        if (!user) return;
+        fetchHabits();
+        const channel = `databases.${DATABASE_ID}.collections.${HABITS_COLLECTION_ID}`;
+        const habitSubscription = client.subscribe(
+            channel,
+            (response: RealtimeResponse) => {
+                if (
+                    response.events.includes(
+                        "databases.*.collections.*.documents.*.create"
+                    ) ||
+                    response.events.includes(
+                        "databases.*.collections.*.documents.*.update"
+                    ) ||
+                    response.events.includes(
+                        "databases.*.collections.*.documents.*.delete"
+                    )
+                )
+                    fetchHabits(); // for the sake of simplicity, we are fetching all habits again
+            }
+        );
+        return () => {
+            habitSubscription();
+        };
+    }, [user, fetchHabits]);
+
     return (
         <View style={styles.container}>
-            <Text>Welcome to the Home Page!</Text>
-            <Text>Navigate to the Login page from here.</Text>
-            <Button mode="text" onPress={signOut} icon={"logout"}>
-                Sign Out
-            </Button>
+            <View style={styles.header}>
+                <Text variant="headlineSmall">Todays Habits</Text>
+                <Button mode="text" onPress={signOut} icon={"logout"}>
+                    Sign Out
+                </Button>
+            </View>
+            <ScrollView showsVerticalScrollIndicator={false}>
+                {habits.length > 0 ? (
+                    habits.map((habit, key) => (
+                        <Surface
+                            key={key}
+                            style={[styles.card, false && styles.cardCompleted]}
+                            elevation={0}
+                        >
+                            <View style={styles.cardContent} key={key}>
+                                <Text style={styles.cardTitle}>
+                                    {" "}
+                                    {habit.title}
+                                </Text>
+                                <Text style={styles.cardDescription}>
+                                    {habit.description}
+                                </Text>
+                                <View style={styles.cardFooter}>
+                                    <View style={styles.streakBadge}>
+                                        <FontAwesome5
+                                            name="fire"
+                                            size={18}
+                                            color="#ff9800"
+                                        />
+                                        <Text style={styles.streakText}>
+                                            {habit.streak_count} day streak
+                                        </Text>
+                                    </View>
+                                    <View style={styles.frequencyBadge}>
+                                        <Text style={styles.frequencyText}>
+                                            {habit.frequency
+                                                .charAt(0)
+                                                .toUpperCase() +
+                                                habit.frequency.slice(1)}
+                                        </Text>
+                                    </View>
+                                </View>
+                            </View>
+                        </Surface>
+                    ))
+                ) : (
+                    <View style={styles.emptyState}>
+                        <Text style={styles.emptyStateText}>
+                            No Habits yet. Add your first Habit!
+                        </Text>
+                    </View>
+                )}
+            </ScrollView>
         </View>
     );
 }
