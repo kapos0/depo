9cb0da0bd8f5600f6bab65021e26982d22e6d13c
Mehmet Enes Turhan <eden.turhann@gmail.com>
2025-06-29 16:42:36 +0300

    I messed up, nothing works, I'll start from scratch

--- mern-stack-starter/backend/src/routes/base.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { toNodeHandler } from "better-auth/node";
-import { setupAuth, auth } from "../lib/auth.js";
-
-const router = express.Router();
-await setupAuth();
-router.all("/auth/{*better-auth}", toNodeHandler(auth));
-export default router;
--- mern-stack-thinkboard/backend/src/controllers/NoteController.ts
+++
@@ -1,6 +0,0 @@
-import { connectDB } from "../lib/connectDB";
-
-export async function getCollection(collectionName: string) {
-    const db = await connectDB();
-    return db.collection(collectionName);
-}
